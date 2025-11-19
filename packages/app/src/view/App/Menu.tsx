2caff9a9215b4576b0183aa5db25e5d81908e7b5
unyoungwax <unyoungwax@hotmail.com>
2025-11-19 10:45:03 +0800

    update

--- packages/app/src/view/App/Menu.tsx
+++ packages/app/src/view/App/Menu.tsx
@@ -1,8 +1,15 @@
-import { LinkLocal } from "@web/core";
-import { useMemo } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { Button, LinkLocal } from "@web/core";
+import { useCallback, useMemo, useState } from "react";
 import { Pathname } from "../../entity/Pathname";
 
 import styles from "./Menu.module.scss";
+
+export type MenuEntry = {
+  readonly href: string;
+  readonly label: string;
+  readonly children?: MenuEntry[];
+};
 
 export type MenuProps = {
   className?: string;
@@ -11,7 +18,7 @@ export type MenuProps = {
 export function Menu(props: MenuProps) {
   const { className } = props;
 
-  const items = useMemo(() => {
+  const entries: MenuEntry[] = useMemo(() => {
     return [{
       href: Pathname.Base,
       label: "Base",
@@ -21,9 +28,13 @@ export function Menu(props: MenuProps) {
     }, {
       href: Pathname.Solana.Wallet,
       label: "Solana",
-    }, {
-      href: Pathname.Solana.Pda,
-      label: "PDA",
+      children: [{
+        href: Pathname.Solana.Wallet,
+        label: "Wallet",
+      }, {
+        href: Pathname.Solana.Pda,
+        label: "PDA",
+      }],
     }, {
       href: Pathname.Time,
       label: "Time",
@@ -34,18 +45,65 @@ export function Menu(props: MenuProps) {
   }, []);
 
   return (
+    <MenuItems entries={entries} className={className} />
+  );
+}
+
+function MenuItems(props: {
+  entries: MenuEntry[];
+  className?: string;
+}) {
+  const { entries, className } = props;
+
+  return (
     <div className={className}>
       {
-        items.map((item) => (
-          <LinkLocal
-            key={item.href}
-            href={item.href}
-            className={styles.item}
-          >
-            { item.label }
-          </LinkLocal>
-        ))
+        entries.map((entry) => {
+          if (entry.children == null || entry.children.length <= 0) {
+            return (
+              <LinkLocal
+                key={entry.href}
+                href={entry.href}
+                className={styles.item}
+              >
+                { entry.label }
+              </LinkLocal>
+            );
+          }
+
+          return (
+            <Submenu key={entry.href} entry={entry} />
+          );
+        })
       }
     </div>
+  );
+}
+
+function Submenu(props: {
+  entry: MenuEntry;
+}) {
+  const { entry } = props;
+
+  const [visible, setVisible] = useState(false);
+
+  const handleToggle = useCallback(() => {
+    setVisible((state) => !state);
+  }, []);
+
+  return (
+    <>
+      <Button onClick={handleToggle} className={styles.item}>
+        <span>{ entry.label }</span>
+        <KeyboardArrowDownIcon fontSize="inherit" />
+      </Button>
+      {
+        visible && (
+          <div className={styles.container}>
+            <MenuItems entries={entry.children ?? []} className={styles.content} />
+          </div>
+        )
+      }
+    </>
   );
 }
